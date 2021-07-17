c37d635516ca616532d2c3ae92b900bfc5579587
Iris6373 <75437488+Iris6373@users.noreply.github.com>
2021-07-17 17:30:32 +0530

    Create A.cpp

---
+++ Codeforces Round #726 (Div. 2)/A.cpp
@@ -0,0 +1,99 @@
+// Problem : https://codeforces.com/contest/1537/problem/A
+
+// Iris_6373
+
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef vector<ll> vt;
+typedef pair<ll, ll> pr;
+typedef vector<pr> vp;
+typedef map<ll, ll> mp;
+#define F first
+#define S second
+#define mp make_pair
+#define l(x) (int)(x).length()
+#define pb push_back
+#define sz(x) (int)(x).size()
+#define all(c) (c).begin(), (c).end()
+#define uni(a) (a).erase(unique(all(a)), (a).end())
+#define prec(n) fixed << setprecision(n)
+#define bitcount(n) __builtin_popcountll(n)
+#define gcd(x, y) __gcd(x, y)
+#define deb(x) cout << #x << " is " << x << "\n"
+#define fo(i, a, b, s) for (int i=(a); (s)>0?i<(b):i>(b); i+=(s))
+#define f(e) fo(i, 0, e, 1)
+#define f1(i, e) fo(i, 0, e, 1)
+#define f2(i, a, b) fo(i, a, b, 1)
+const int mod = 1000000007;
+auto clk = clock();
+
+#ifndef ONLINE_JUDGE
+#define de(x) cout << #x << " = "; _print(x); cout << "\n";
+#else
+#define de(x)
+#endif
+
+void _print(ll t) {cout << t;}
+void _print(int t) {cout << t;}
+void _print(string t) {cout << t;}
+void _print(char t) {cout << t;}
+void _print(ld t) {cout << t;}
+void _print(double t) {cout << t;}
+void _print(ull t) {cout << t;}
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cout << "{"; _print(p.F); cout << ","; _print(p.S); cout << "}";}
+template <class T> void _print(vector <T> v) {cout << "[ "; for (T i : v) {_print(i); cout << " ";} cout << "]";}
+template <class T> void _print(set <T> v) {cout << "[ "; for (T i : v) {_print(i); cout << " ";} cout << "]";}
+template <class T> void _print(multiset <T> v) {cout << "[ "; for (T i : v) {_print(i); cout << " ";} cout << "]";}
+template <class T, class V> void _print(map <T, V> v) {cout << "[ "; for (auto i : v) {_print(i); cout << " ";} cout << "]";}
+
+void solve() {  
+    ll i, j, k, m, n, x=0;
+    string s1, s2;
+    cin >> n;
+    vt v(n);
+    f(n) {
+        cin >> v[i];
+        x += v[i];
+    }
+    ld y=0;
+    y = x;
+    y /= n;
+    if (y == 1) cout << "0";
+    else if (y > 0) {
+        if (x > n) {
+            cout << x-n;
+        }
+        else {
+            cout << 1;
+        }
+    }
+    else if (y == 0) {
+        cout << 1;
+    }
+    else {
+        cout << 1;
+    }
+    cout << "\n";
+}
+
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    ll t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
